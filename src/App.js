6f6eed6723350505853138048ecd5bd08fe8fd81
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-16 10:13:29 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import {BrowserRouter as Router} from 'react-router-dom'
 const App=()=>{
     return(
-          <div className="alert alert-info">
-             Just Checking....
-          </div>
+  <Router>
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+          </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </Router>
     )
 }
 
